301ec2f6897a94d871a818879985f294484061a0
Katarzyna Kabala <katarzyna_kabala@o2.pl>
2021-10-30 19:40:05 +0200

    Else If Statements

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-var youLikeMeat = false;
-if(youLikeMeat){
-    document.write("you like meat");
-} else{
-    document.write("you hate meat");
+var myAge = 25;
+if (myAge > 30){
+    document.write("you are over 30!");
+} else if (myAge > 20){
+    document.write("you are over 20!");
+} else if (myAge > 10){
+    document.write("you are over 10!");
+} else {
+    document.write("you are not over 10!");
 }
