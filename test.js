1a1284a9fdab5f5257d8612da8376b8d1e37675c
Katarzyna Kabala <katarzyna_kabala@o2.pl>
2021-11-02 16:54:08 +0100

    NaN

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-var a = 7;
+var a = 6;
 var b = 5;
 
-console.log(a + b);
-console.log(typeof (a + b) );
-console.log(Math.round(7.8));
-console.log(Math.round(7.4));
-console.log(Math.floor(7.8));
-console.log(Math.ceil(7.2));
-console.log(Math.max(7, 4, 9, 8));
+if (isNaN(a)){
+    console.log("that aint even a number");
+}
+else{
+    console.log("meaning of life is " + (a * b));
+}
