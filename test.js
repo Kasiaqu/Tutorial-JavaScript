51287c69c8462cb353d0a76f01456566045f7839
Katarzyna Kabala <katarzyna_kabala@o2.pl>
2021-11-03 11:20:33 +0100

    The Date Object

--- test.js
+++ test.js
@@ -1,19 +1,17 @@
-var Car = function(maxSpeed, driver){
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.drive = function(speed, time){
-        console.log(speed * time);
-    };
-    this.logDriver = function(){
-        console.log("driver name is " + this.driver);
-    };
+var birthday = new Date (1985, 0, 15, 11, 15, 25);
+var birthday2 = new Date (1985, 0, 15, 11, 15, 25);
+
+console.log(birthday.getMonth());
+console.log(birthday.getFullYear());
+console.log(birthday.getDate());
+console.log(birthday.getDay());
+console.log(birthday.getHours());
+console.log(birthday.getTime());
+
+if(birthday.getTime() == birthday2.getTime()){
+    console.log("birthday are equal");
+} else{
+    console.log("birthday are not equal");
 }
 
-var myCar = new Car (70, "Ninja Man");
-var myCar2 = new Car (40, "Humpty Dumpty");
-var myCar3 = new Car (10, "Shaun");
-var myCar4 = new Car (90, "James Bond");
-
-myCar3.logDriver();
-myCar2.drive(50, 3);
-
+ 
